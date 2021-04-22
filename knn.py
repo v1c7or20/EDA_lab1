204b9b91e0025fb606742a889ecec1683f535ed2
johantv <johan.tanta@utec.edu.pe>
2021-04-21 19:09:56 -0500

    completed

--- knn.py
+++ knn.py
@@ -1,6 +1,6 @@
 from sklearn.neighbors import KNeighborsClassifier
 import time
-
+import random
 features = []
 labels = []
 def read_dataset(name):
@@ -12,21 +12,24 @@ def read_dataset(name):
         labels.append(x+", "+y)
     f.close()
 
-read_dataset("d1.txt")
-k = int(input("Numero de vecinos: "))
-inp = input("Ingresa coordenadas separadas por espacio: ")
-x, y = inp.split()
-total_time = time.time()
-model = KNeighborsClassifier(n_neighbors=k)
-fit_time = time.time()
-model.fit(features,labels)
-fit_time = time.time() - fit_time
-knn_time = time.time()
-predicted= model.kneighbors([[int(x), int(y)]])
-knn_time = time.time() - knn_time
-total_time = time.time() - total_time
-print("--- Fit time: %s seconds ---" % (fit_time))
-print("--- KNN time: %s seconds ---" % (knn_time))
-print("--- Total time: %s seconds ---" % (total_time))
-for prediction in predicted[1][0]:
-    print(labels[prediction])
+read_dataset("dataset_a.txt")
+#k = int(input("Numero de vecinos: "))
+tests = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 50000]
+for k in tests:
+    x = random.randint(0, 1500)
+    y = random.randint(0, 1500)
+    total_time = time.time()
+    model = KNeighborsClassifier(n_neighbors=k)
+    fit_time = time.time()
+    model.fit(features,labels)
+    fit_time = time.time() - fit_time
+    knn_time = time.time()
+    predicted= model.kneighbors([[x, y]])
+    knn_time = time.time() - knn_time
+    total_time = time.time() - total_time
+    #print("--- Fit time: %s seconds ---" % (fit_time))
+    #print("--- KNN time: %s seconds ---" % (knn_time))
+    #print("--- Total time: %s seconds ---" % (total_time))
+    print(total_time, end=", ")
+#for prediction in predicted[1][0]:
+ #   print(labels[prediction])
--- lab1.cpp
+++ lab1.cpp
@@ -22,13 +22,12 @@ void dataset(int cantidad, int min, int max, string path){
 void create_datasets(){
     dataset(50000, 0, 901, "dataset_a.txt");
     dataset(25000, 0, 901, "dataset_b.txt");
-    dataset(25000, 900, 1501, "dataset_b.txt");
+    dataset(25000, 900, 601, "dataset_b.txt");
     dataset(50000, 0, 1501, "dataset_c.txt");
     return;
 }
-//vector<int> cols()
-list<pair<int,int>> todo_list;
 
+list<pair<int,int>> todo_list;
 
 void parsing3(string dataset){
     fstream file;
@@ -64,42 +63,24 @@ void k_near_list(int x, int y, int k){
     
 }
 
-void k_near(int x, int y, int k){
-    //int total_cord = todo[x][y];
-    bool max_x, min_x, max_y, min_y;
-    for(int i = 1; i <= 1500; ++i){
-        max_x = x+i <= 1500;
-        min_x = x-i >= 0;
-        max_y = y+i <= 1500;
-        min_y = y-i >= 0;
-        
-        // if(max_x && todo[x+i][y] != 0)
-        // if(min_x && todo[x-i][y] != 0)
-        // if(max_y && todo[x][y+i] != 0)
-        // if(min_y && todo[x][y-i] != 0)
-        // if(max_x && max_y && todo[x+i][y+i] != 0)
-        // if(max_x && min_y && todo[x+i][y-i] != 0)
-        // if(min_x && min_y && todo[x-i][y-i])
-        // if(min_x && max_y && todo[x-i][y+i])
-    }
-    return;
-}
-
 int main(int argc, char const *argv[]){
     srand(time(NULL));
-    parsing3("dataset_a.txt");
-    for (int i = 0; i < 10; i++)
+    parsing3("dataset_c.txt");
+    int tests[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 50000};
+
+    for (int i = 0; i <= 16; i++)
     {
-        int x=rand()%900 + 0;
-    int y=rand()%900 + 0;
-    int k=rand()%900 + 0;
+        int x=rand()%1500 + 0;
+        int y=rand()%1500 + 0;
+        int k=tests[i];
     auto start = high_resolution_clock::now();
     k_near_list(x, y, k);
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
   
-    cout << "Time taken by function: "
-         << duration.count() << " microseconds" << endl;
+    cout << duration.count() << ", ";
+    //cout << "Time taken by function: "
+    //     << duration.count() << " microseconds" << endl;
     
     }
     return 0;
@@ -107,17 +88,3 @@ int main(int argc, char const *argv[]){
 
 // https://www.overleaf.com/1431967612zbkczmvmtkcv
 // scikit-learn
-
-/* tiempos microsegundos
-6913
-6916
-7241
-6896
-7110
-7107
-6913
-6992
-6916
-7040
-6955
-*/
